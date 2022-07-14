b0068346e2550a935ccfca451b3779412f28cafe
Kevin Rocker <kevin.j.rocker@gmail.com>
2022-07-14 18:42:44 +0200

    Importing from Static

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,12 @@
+<script lang="ts">
+    let xml = '';
+    fetch("/ACS/Settings/AreaTypeDef/AreaTypeDef.xml")
+        .then(val=>val.blob())
+        .then(blob=>blob.text())
+        .then(text=> xml = text)
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{xml}
