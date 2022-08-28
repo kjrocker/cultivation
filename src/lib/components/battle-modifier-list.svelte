179b5f96f3e244009e840de1249319ac59a69105
Kevin Rocker <kevin.j.rocker@gmail.com>
2022-08-28 00:23:01 -0400

    Battle modifiers and UI tweaks

--- src/lib/components/battle-modifier-list.svelte
+++ src/lib/components/battle-modifier-list.svelte
@@ -1,28 +1,37 @@
 <script lang="ts">
 	import { PropertyConfiguration } from '$lib/data/property-config';
+
 	import { getAllModifierGroups } from '$lib/util/get-all-modifier-groups';
+
 	import Collapse from './base/collapse.svelte';
 	import Panel from './base/panel.svelte';
 	import PropertyBonusListItem from './property-bonus-list-item.svelte';
+	import QiRecovery from './qi-recovery.svelte';
 	import { partModifierStore, secretBodyModifierStore } from './stores/current-modifiers-store';
+
+	const groupList = {
+		BodyPratice_MaxZhenQi: 'Max True Qi',
+		NpcFight_BaseDodgeChance: 'Global Dodge',
+		BodyPratice_BodyStrong: 'Body Strength'
+	};
 
 	$: groups = getAllModifierGroups($secretBodyModifierStore, $partModifierStore);
 </script>
 
 <Panel>
-	<Collapse buttonClasses="py-2 text-lg px-2 w-full justify-between">
-		<slot slot="header">All Modifiers</slot>
+	<Collapse defaultOpen buttonClasses="py-2 text-lg px-2 w-full justify-between">
+		<slot slot="header">Battle Stats</slot>
 		<hr />
-		{#each Object.keys(groups) as name}
+		{#each Object.entries(groupList) as [name, displayName]}
 			<PropertyBonusListItem
 				{name}
-				displayName={`${
-					groups[name].DisplayName ?? PropertyConfiguration[name]?.DisplayName ?? name
-				}`}
+				{displayName}
 				bonuses={groups[name]}
 				percentage={!!PropertyConfiguration[name]?.percentage}
 				rate={!!PropertyConfiguration[name]?.rate}
+				tooltip={false}
 			/>
 		{/each}
+		<QiRecovery class="mx-2 py-1 cursor-default" />
 	</Collapse>
 </Panel>
--- src/lib/util/total-property-item.ts
+++ src/lib/util/total-property-item.ts
@@ -6,16 +6,28 @@ interface PropertyConfig {
 	max?: number;
 	min?: number;
 	base?: number;
+	gAddP?: number;
+	gAddV?: number;
 }
+
+const DefaultConfig: Required<PropertyConfig> = {
+	max: Infinity,
+	min: -Infinity,
+	base: 0,
+	gAddP: 0,
+	gAddV: 0
+};
 
 export const calculatePropertyItem = <Config extends PropertyConfig>(
 	prop?: PartProperty,
-	config?: Config
+	partialConfig?: Config
 ) => {
-	const baseValue = undefinedSum(config?.base, prop?.BAddV);
+	const config: Required<PropertyConfig> = { ...DefaultConfig, ...partialConfig };
+	const baseValue = undefinedSum(config.base, prop?.BAddV);
 	const percentBonus = undefinedSum(1, prop?.AddP);
 	const bonusValue = prop?.AddV ?? 0;
-	const total = baseValue * percentBonus + bonusValue;
-	const clamped = clamp(config?.min ?? -Infinity, config?.max ?? Infinity, total);
+	const total =
+		baseValue * percentBonus * (1 + (config?.gAddP ?? 0)) + bonusValue + (config?.gAddV ?? 0);
+	const clamped = clamp(config.min, config.max, total);
 	return { baseValue, percentBonus, bonusValue, total: clamped };
 };
