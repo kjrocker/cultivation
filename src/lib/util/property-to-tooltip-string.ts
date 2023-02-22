63c35196e421ebb8b13ef7f1a1931a84c79a5181
Kevin Rocker <kevin.j.rocker@gmail.com>
2023-02-22 23:57:34 +0000

    Showing all modifiers in tooltip

--- src/lib/util/property-to-tooltip-string.ts
+++ src/lib/util/property-to-tooltip-string.ts
@@ -5,23 +5,32 @@ export const propertyToString = <T extends PartProperty>(
 	prop: T,
 	level = 1,
 	config?: Partial<{ percentage: boolean; rate: boolean }>
-): string => {
+): string[] => {
+	let result: string[] = [];
 	if (prop.BAddV) {
-		return `Base ${formatNumber(prop.BAddV * level, {
-			...config,
-			precision: 2,
-			plusSign: true
-		})}`;
-	} else if (prop.AddP) {
-		return formatNumber(prop.AddP * level, {
-			percentage: true,
-			rate: false,
-			precision: 2,
-			plusSign: true
-		});
-	} else if (prop.AddV) {
-		return formatNumber(prop.AddV * level, { ...config, precision: 2, plusSign: true });
-	} else {
-		return `No bonus detected, how did that happen?`;
+		result.push(
+			`Base ${formatNumber(prop.BAddV * level, {
+				...config,
+				precision: 2,
+				plusSign: true
+			})}`
+		);
 	}
+	if (prop.AddP) {
+		result.push(
+			formatNumber(prop.AddP * level, {
+				percentage: true,
+				rate: false,
+				precision: 2,
+				plusSign: true
+			})
+		);
+	}
+	if (prop.AddV) {
+		result.push(formatNumber(prop.AddV * level, { ...config, precision: 2, plusSign: true }));
+	}
+	if (result.length === 0) {
+		result.push(`No bonus detected, how did that happen?`);
+	}
+	return result;
 };
