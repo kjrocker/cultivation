2232c9a322710995335fc1fb1053403c7700e4da
Kevin Rocker <kevin.j.rocker@gmail.com>
2023-02-23 00:13:06 +0000

    Hiding qi recovery addP from modifier tooltips

--- src/lib/util/property-to-tooltip-string.ts
+++ src/lib/util/property-to-tooltip-string.ts
@@ -1,36 +1,42 @@
 import { formatNumber } from './format-number';
 import type { PartProperty } from '../data/types';
+import { PropertyBlacklist } from '../data/property-config';
 
-export const propertyToString = <T extends PartProperty>(
-	prop: T,
-	level = 1,
-	config?: Partial<{ percentage: boolean; rate: boolean }>
-): string[] => {
-	let result: string[] = [];
-	if (prop.BAddV) {
-		result.push(
-			`Base ${formatNumber(prop.BAddV * level, {
-				...config,
-				precision: 2,
-				plusSign: true
-			})}`
-		);
-	}
-	if (prop.AddP) {
-		result.push(
-			formatNumber(prop.AddP * level, {
-				percentage: true,
-				rate: false,
-				precision: 2,
-				plusSign: true
-			})
-		);
-	}
-	if (prop.AddV) {
-		result.push(formatNumber(prop.AddV * level, { ...config, precision: 2, plusSign: true }));
-	}
-	if (result.length === 0) {
-		result.push(`No bonus detected, how did that happen?`);
-	}
-	return result;
+type PropertyToStringConfig = {
+	blacklist: typeof PropertyBlacklist;
 };
+
+export const createPropertyToString =
+	({ blacklist }: PropertyToStringConfig) =>
+	<T extends PartProperty>(
+		prop: T,
+		level = 1,
+		config?: Partial<{ percentage: boolean; rate: boolean }>
+	): string[] => {
+		let result: string[] = [];
+		if (prop.BAddV && !blacklist[prop.Name]?.BAddV) {
+			result.push(
+				`Base ${formatNumber(prop.BAddV * level, {
+					...config,
+					precision: 2,
+					plusSign: true
+				})}`
+			);
+		}
+		if (prop.AddP && !blacklist[prop.Name]?.AddP) {
+			result.push(
+				formatNumber(prop.AddP * level, {
+					percentage: true,
+					rate: false,
+					precision: 2,
+					plusSign: true
+				})
+			);
+		}
+		if (prop.AddV && !blacklist[prop.Name]?.AddV) {
+			result.push(formatNumber(prop.AddV * level, { ...config, precision: 2, plusSign: true }));
+		}
+		return result;
+	};
+
+export const propertyToString = createPropertyToString({ blacklist: PropertyBlacklist });
